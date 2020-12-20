ad668237368d2ee63677558aed10e6139894154b
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2020-12-20 17:14:13 +0530

    add hero and gem

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
 
 function load_images() {
     // player, virus, gem
+    enemy_image = new Image;
+    enemy_image.src = "Assets/v1.png";
+
+    player_image = new Image;
+    player_image.src = "Assets/superhero.png";
+
+    gem_image = new Image;
+    gem_image.src = "Assets/gemm.png";
 }
 
 
@@ -16,12 +24,46 @@ function init() {
     // create a context
     pen = canvas.getContext('2d');
 
-    box = {
+    e1 = {
         x : 150,
         y : 50,
         w : 60,
         h : 60,
         speed : 20,
+    };
+
+    e2 = {
+        x: 300,
+        y: 150,
+        w: 60,
+        h: 60,
+        speed: 30,
+    };
+
+    e3 = {
+        x: 450,
+        y: 20,
+        w: 60,
+        h: 60,
+        speed: 40,
+    };
+
+    enemy = [e1, e2, e3];
+
+    player = {
+        x: 20,
+        y: H/2,
+        w: 60,
+        h: 60,
+        speed: 20,
+        // moving : false,
+    };
+
+    gem = {
+        x: W-100,
+        y: H/2,
+        w: 60,
+        h: 60,
     };
 }
 
@@ -30,15 +72,28 @@ function draw() {
     pen.clearRect(0, 0, W, H);
 
     pen.fillStyle = "red";
-    pen.fillRect(box.x, box.y, box.w, box.h);
+    // pen.fillRect(box.x, box.y, box.w, box.h);
+    // pen.drawImage(enemy_image, box.x, box.y, box.w, box.h);
+
+    //draw the player
+    pen.drawImage(player_image, player.x, player.y, player.w, player.h);
+
+    //draw the gem
+    pen.drawImage(gem_image, gem.x, gem.y, gem.w, gem.h);
+
+    for (let i=0; i<enemy.length; i++) {
+        pen.drawImage(enemy_image, enemy[i].x, enemy[i].y, enemy[i].w, enemy[i].h);
+    }
 }
 
 function update() {
     // move the box downwards
-    box.y += box.speed;
-
-    if (box.y > H - box.h || box.y < 0) {
-        box.speed *= -1;
+    // update each enemy by the same logic (using loop)
+    for (let i=0; i<enemy.length; i++) {
+        enemy[i].y += enemy[i].speed;
+        if (enemy[i].y > H-enemy[i].h || enemy[i].y < 0) {
+            enemy[i].speed *= -1;
+        }
     }
 }
 
