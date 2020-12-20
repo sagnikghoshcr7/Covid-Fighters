b599fe2dea7433dcecdbeb5b57c619fa3c0f2bef
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2020-12-20 16:03:17 +0530

    add moving virus

--- game.js
+++ game.js
@@ -20,16 +20,26 @@ function init() {
         x : 150,
         y : 50,
         w : 60,
-        h : 60
-    }
+        h : 60,
+        speed : 20,
+    };
 }
 
 function draw() {
+    //clear the canvas area for the old frame
+    pen.clearRect(0, 0, W, H);
 
+    pen.fillStyle = "red";
+    pen.fillRect(box.x, box.y, box.w, box.h);
 }
 
 function update() {
+    // move the box downwards
+    box.y += box.speed;
 
+    if (box.y > H - box.h || box.y < 0) {
+        box.speed *= -1;
+    }
 }
 
 function gameloop() {
